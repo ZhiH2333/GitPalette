15095f44cf054b0818f02384ff47c40cbd6d9d24
ZhiH2333 <166639072@qq.com>
2026-08-06 13:20:50 +0800

    Initial Commit

---
+++ GitPalette/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitPalette
+//
+//  Created by Evan on 2026/8/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitPalette/GitPaletteApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitPaletteApp.swift
+//  GitPalette
+//
+//  Created by Evan on 2026/8/6.
+//
+
+import SwiftUI
+
+@main
+struct GitPaletteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
